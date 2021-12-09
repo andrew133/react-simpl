39591eef74be3ef31e4ab232fd9d0538e88c8f9a
Andrey <55235312+andrew133@users.noreply.github.com>
2021-12-09 15:13:54 +0300

    07

--- src/App.tsx
+++ src/App.tsx
@@ -10,24 +10,12 @@ import {UncontrolledRating} from "./components/Rating/UncontrolledRating";
 function App() {
     debugger
     return (
-        <div>
-            {/*<PageTitle title ={"This is component"} />*/}
-            {/*<PageTitle title ={"My friend"} />*/}
-            {/*<Accordion AccTitle={"Menu"} AccBody1={"js"}*/}
-            {/*           AccBody2={"css"} AccBody3={"html"} collapsed={true}/>*/}
-            {/*<Accordion AccTitle={"User"} AccBody1={"1"} AccBody2={"2"} AccBody3={"3"}*/}
-            {/*           collapsed={false}/>*/}
-            {/*<Rating value={0}/>*/}
-            {/*<Rating value={1}/>*/}
-            {/*<Rating value={2}/>*/}
-            {/*<Rating value={3}/>*/}
-            {/*<Rating value={4}/>*/}
-            {/*<Rating value={5}/>*/}
+        <div className={"App"}>
+
             <OnOff />
-            <OnOff />
-            <OnOff />
+
             <UncontrolledAccordion AccTitle={"Menu"}/>
-           <UncontrolledAccordion AccTitle={"User"}/>
+
             <UncontrolledRating />
 
         </div>
